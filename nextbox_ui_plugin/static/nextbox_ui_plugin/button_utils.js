94d0c1a230193c628a713c388616bdf050ede653
Kenneth Blossom's Portfolio <33361957+mscrnt@users.noreply.github.com>
2023-09-13 15:30:22 -0700

    Update button_utils.js

--- nextbox_ui_plugin/static/nextbox_ui_plugin/button_utils.js
+++ nextbox_ui_plugin/static/nextbox_ui_plugin/button_utils.js
@@ -18,7 +18,19 @@ function showHideUndonnectedButtonOnClick(button) {
 
 function layerSelectorOnChange(checkbox){
     showHideDeviceRoles(checkbox.value, checkbox.checked);
+    
+    var topologyContainer = document.getElementById('nx-ui-topology');
+
+    // Add or remove the custom CSS classes based on the selected layer
+    if (checkbox.value === 'value_for_layer_1') {  // replace with the actual value for layer 1
+        topologyContainer.classList.add('n-topology', 'layer-background-1');
+        topologyContainer.classList.remove('layer-background-2');
+    } else if (checkbox.value === 'value_for_layer_2') {  // replace with the actual value for layer 2
+        topologyContainer.classList.add('n-topology', 'layer-background-2');
+        topologyContainer.classList.remove('layer-background-1');
+    }
 };
+
 
 function layerSelectorByTagOnChange(checkbox){
     showHideDevicesByTag(checkbox.value, checkbox.checked)
